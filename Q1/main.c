eecaa124cf7bb39d4bb797bf013a0e3d19a7f376
SurriyaLuavan <surriyaluavan@gmail.com>
2023-01-25 11:16:07 -0400

    Completed and tested getSquares function

--- Q1/main.c
+++ Q1/main.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
-#include <math.h>
 
 int getSquares(int, int);
 
-void main() {
-    getSquares(6, 5);
+int main() {
+    getSquares(16, 3);
     return 0;
 }
 
-void getSquares(int N, int M) {
+int getSquares(int N, int M) {
+    
+    int side_length = 0;
+    int squares = 0;
 
+    if (N == M) {
+    	printf("%dx%d", N, N);
+    }
+    else {
+    	 while (N != 0 && M != 0) {
+    		if (N > M) {
+    			side_length = M;
+
+    			// calculate number of squares that can be cut from paper
+    			squares = ((N * M) / (side_length * side_length));
+    			N = N % side_length;
+    		} else {
+    			side_length = N;
+
+    			squares = ((N * M) / (side_length * side_length));
+    			M = M % side_length;
+    		}
+
+    		for (int i = 0; i < squares; i++) {
+    			printf("%dx%d", side_length, side_length);
+    			if((N != 0 && M != 0) || (i != (squares-1))) {
+    				printf(", ");
+    			}
+    		}
+    	}
+    }
     return 0;
 }
